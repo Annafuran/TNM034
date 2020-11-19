859634f0a90ea1ccf54e58b4f1f49cb585434c54
hennil93 <henni317@student.liu.se>
2020-11-19 11:31:36 +0100

    Localized average eye position and cropped image accordingly

--- main.m
+++ main.m
@@ -16,8 +16,8 @@
 %
 %im: Image of unknown face, RGB-image in uint8 format in the range [0, 255]
 %
-%id: The identity number (integer) of the indetified person, i.e. `1ï¿½,
-%`2ï¿½,..., `16ï¿½ for the persons belonging to `db1ï¿½and `0ï¿½for all other
+%id: The identity number (integer) of the indetified person, i.e. `1ï¿½ï¿½ï¿½,
+%`2ï¿½ï¿½ï¿½,..., `16ï¿½ï¿½ï¿½ for the persons belonging to `db1ï¿½ï¿½ï¿½and `0ï¿½ï¿½ï¿½for all other
 %faces. 
 %
 %Your program code. 
@@ -28,16 +28,45 @@
 clear 
 clc
 
-%Flyttade ut bilden för fick den inte att läsa från mappen (?!)
-refImg = imread('db0_1.jpg');
+%Flyttade ut bilden fï¿½r fick den inte att lï¿½sa frï¿½n mappen (?!)
+refImg = imread('DB1/DB1/db1_08.jpg');
 refImg = im2uint8(refImg);
 
 %Lightning Compensation
 %refImg = GrayWorld(refImg);
 refImg = WhitePatch(refImg);
-%refImg = EyeDetection(refImg);
-refImg = MouthDetection(refImg);
 
-imshow(refImg);
+EyeMask = EyeDetection(refImg);
+MouthMask = MouthDetection(refImg);
+FaceMask = SkinColorDetection(refImg);
+
+mask = EyeMask.*FaceMask;
+
+%Crop the image according to the position of the eyes
+
+thresholded = imbinarize(mask,0.6);
+
+n1 = thresholded(:, 1 : round(end/2));
+n2 = thresholded(:, round(end/2+1) : end );
+
+[y,x] = find(n1);
+xmeanN1 = mean(x);
+ymeanN1 = mean(y);
+
+[y,x] = find(n2);
+xmeanN2 = mean(x);
+ymeanN2 = mean(y);
+
+EyeXAvg = (xmeanN1 + (width(n1) + 1 + xmeanN2)) / 2;
+EyeYAvg = ((ymeanN1+ymeanN2) / 2)*(7/5);
+
+targetSize = [426 362]; %minsta height och width
+
+rect = [EyeXAvg-targetSize(2)/2 EyeYAvg-targetSize(1)/2 targetSize(2) targetSize(1)];
+imcrop(mask, rect);
+
+%imshow(mask);
+
+
 
 
