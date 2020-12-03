2dd33ef36350019256a00aef3aa2d6f440920681
hennil93 <henni317@student.liu.se>
2020-12-03 11:22:06 +0100

    Changed the algorithm for localizing the eyes

--- DetectFace.m
+++ DetectFace.m
@@ -1,6 +1,6 @@
 function [faceImg] = DetectFace(refImg)
     
-    %refImg = imread('DB1/DB1/db1_13.jpg');
+    %refImg = imread('DB1/DB1/db1_04.jpg');
     refImg = WhitePatch(refImg);
 
     EyeMask = EyeDetection(refImg);
@@ -15,23 +15,17 @@ function [faceImg] = DetectFace(refImg)
     [y,x] = find(thresholdedMouth);
     ymeanMouth = round(mean(y));
 
+    %Localize the position of the eyes
+    [Eye1x, Eye1y, Eye2x, Eye2y] = LocalizeEyes(mask, ymeanMouth);
+    
+    %Rotate the image to align the y-position of the eyes
+    mask = RotateFace(mask, Eye1x, Eye1y, Eye2x, Eye2y);
+    [Eye1x, Eye1y, Eye2x, Eye2y] = LocalizeEyes(mask, ymeanMouth);
+
+    EyeXAvg = (Eye1x+Eye2x)/2;
+    EyeYAvg = ((Eye1y+Eye2y)/2)*(6/5);
+    
     %Crop the image according to the position of the eyes
-    thresholdedEyes = imbinarize(mask(1 : (ymeanMouth-35), :),max(max(mask))-0.4);
-
-    n1 = thresholdedEyes(:, 1 : round(end/2));
-    n2 = thresholdedEyes(:, round(end/2+1) : end );
-
-    [y,x] = find(n1);
-    xmeanN1 = mean(x);
-    ymeanN1 = mean(y);
-
-    [y,x] = find(n2);
-    xmeanN2 = mean(x);
-    ymeanN2 = mean(y);
-
-    EyeXAvg = (xmeanN1 + (width(n1) + 1 + xmeanN2)) / 2;
-    EyeYAvg = ((ymeanN1+ymeanN2) / 2)*(11/10);
-
     targetSize = [300 400]; %minsta height och width
 
     rect = [round(EyeXAvg-targetSize(1)/2) round(EyeYAvg-targetSize(2)/2) targetSize(1) targetSize(2)];
