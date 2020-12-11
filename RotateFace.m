30393550ad20de503c8611b5ca92c816e3448654
hennil93 <henni317@student.liu.se>
2020-12-11 15:40:38 +0100

    Made sure that we always rotate according to which eye is the highest

--- RotateFace.m
+++ RotateFace.m
@@ -3,10 +3,18 @@ function [rotatedFace] = RotateFace(inputImg, eye1x, eye1y, eye2x, eye2y)
 deltaX = abs(eye2x-eye1x);
 deltaY = abs(eye2y-eye1y);
 
+if (eye1x > eye2x)
+    eyeRy = eye1y;
+    eyeLy = eye2y;
+else
+    eyeRy = eye2y;
+    eyeLy = eye1y;
+end
+
 %Rotera medurs om eye1y är större än eye2y, annars rotera moturs
-sign = -1;
-if(eye1y < eye2y)
-    sign = 1;
+sign = 1;
+if(eyeLy > eyeRy)
+    sign = -1;
 end
   
 theta = sign*(atan2(deltaY,deltaX));
